e2078985db0cad418276c1f22416d0b93d5a0fb6
Hang Zhou <zhou_hang@berkeley.edu>
2024-05-08 23:50:28 -0400

    inital commit

---
+++ email_scrapper/go.mod
@@ -0,0 +1,8 @@
+module email_scrapper
+
+go 1.15
+
+require (
+	github.com/emersion/go-imap v1.2.1
+	github.com/emersion/go-imap/v2 v2.0.0-beta.3 // indirect
+)
---
+++ email_scrapper/service/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+import (
+    imap "github.com/emersion/go-imap"
+)
+
+func main() {
+    // a spinner that periodically send request to email server
+    // save the data to no-sql db and send a message to analyzer worker
+
+    // * find a way to get email service more reponsively and lightweight
+
+    fmt.Println("Hello World")
+}
+
